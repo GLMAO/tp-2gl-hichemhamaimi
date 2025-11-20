f5523a61891f001e3b53e389243a9f87d44cf737
hichem <leetabrahamcode@gmail.com>
2025-11-20 13:56:53 +0100

    completed lab

--- src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
+++ src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
@@ -7,37 +7,37 @@ public class GestionnaireEmploiDuTemps implements Subject {
     private List<ICours> listeCours = new ArrayList<>();
     private List<Observer> observers = new ArrayList<>();
 
+    @Override
+    public void attach(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+
     public void ajouterCours(ICours cours) {
         this.listeCours.add(cours);
         System.out.println("Nouveau cours ajouté : " + cours.getDescription());
-        // TODO: C'est ici qu'il faudrait notifier les étudiants (Observer pattern)
-        notifyObservers("a new course was added! : " + cours.getDescription())
+        notifyObservers("Nouveau cours ajouté : " + cours.getDescription());
     }
 
     public void modifierCours(ICours cours, String message) {
         // Logique de modification...
         System.out.println("Cours modifié : " + message);
-        // TODO: Notifier les observateurs ici aussi
-        notifyObservers("the course " + cours.getDescription() + " has been modified")
+        notifyObservers("Cours modifié : " + message);
     }
 
-    public void setChangement(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setChangement'");
+    public void setChangement(String message) {
+        System.out.println("Changement dans l'emploi du temps : " + message);
+        notifyObservers(message);
     }
-
-    public void attach(Observer o){
-        observers.add(o);
-    }
-
-    public void detach(Observer o){
-        observers.remove(o);
-    }
-    
-    public void notifyObservers(String message){
-        for(Observer o : observers){
-            observer.update(message);
-        }
-    }
-
 }
